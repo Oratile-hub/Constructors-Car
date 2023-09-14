faeb0154ef4069f6c193779088712e5ab814c040
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-09-14 11:13:55 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_Car
+{
+    internal class Car
+    {
+
+        public string model;
+        public string colour;
+        public int year;
+
+        //constructor
+
+        public Car(string aModel, string aColour, int aYear)
+        {
+            model = aModel;
+            colour = aColour;
+            year = aYear;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_Car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car("Mercedis Benz", "Mate Black", 2023);
+
+            Console.WriteLine(car1.model);
+            Console.WriteLine(car1.colour);
+            Console.WriteLine(car1.year);
+            Console.ReadLine();
+        }
+    }
+}
